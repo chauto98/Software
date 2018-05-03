26c0140a9859d42170a1aea3a0992e044b3fca25
chauto98 <marconlala@hotmail.com>
2018-05-03 17:57:54 -0300

    descarga esta wea

--- catkin_ws/src/ros_cap/tutorial/ojo_magico.py
+++ catkin_ws/src/ros_cap/tutorial/ojo_magico.py
@@ -5,19 +5,26 @@ from sensor_msgs.msg import Image
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
-
+fx=167.856
+fy=169.605
+cx=152.939
+cy=111.756
+Dpx=3.5
+Dpy=3
 
 class buscadores(object):
 	def __init__(self,args):
          	#def publicar(self):
 		self.args = args
-		super(buscadores,self).__init__()
+		super(buscadores,self).__init__()		
+		self.publisher = rospy.Publisher("/geometry_msg/Point",Image, queue_size=10)		
 		self.publisher = rospy.Publisher("/godduck",Image, queue_size=10)
                 self.subscriber = rospy.Subscriber("/duckiebot/camera_node/image/rect",Image,self.callback)
 		self.bridge = CvBridge()
 		self.image = Image()
 
 	def callback(self,msg):
+		#deteccion de patos:
 		change = self.bridge.imgmsg_to_cv2(msg,"bgr8")
 		imageinHSV = cv2.cvtColor(change, cv2.COLOR_BGR2HSV) #cambiar entre espacios de colores
 		mask= cv2.inRange(imageinHSV,(20, 100, 150), (35, 255, 255))
@@ -32,8 +39,20 @@ class buscadores(object):
 			if (area>400):
 				x,y,w,h = cv2.boundingRect(contorno)
 				cv2.rectangle(change, (x,y), (x+w,y+h), (0,0,0), 2)
+				#medidor de distancia		
+				z=(fx/(x+w))*Dpx
+				u=x+w/2 
+				v=y+h/2
+				x3=(u-cx)*z/fx
+				y3=(v-cy)*z/fy
+				z3=z
+				
 		msg = self.bridge.cv2_to_imgmsg(change,"bgr8")
-		self.publisher.publish(msg)
+		self.publisher.publish(msg)  
+		self.publisher.publish(z3)
+		#medidor de diostancia:
+
+		
 
 
 def main():
